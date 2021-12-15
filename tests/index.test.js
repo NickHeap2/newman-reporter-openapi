cc87f4edae72d61b573d3d07e2541706d721ed71
Nick Heap <nickheap@gmail.com>
2021-12-15 00:17:53 +0000

    feat: latest updates before publish

--- tests/index.test.js
+++
@@ -1,7 +0,0 @@
-/* globals describe it */
-
-describe('it should get a path', function () {
-  it('should satisfy OpenAPI spec', async function () {
-    
-  })
-})
--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,5 +1,4 @@
 /* globals describe it expect */
-
 const utils = require('../lib/utils')
 
 describe('utils path', function () {
@@ -43,6 +42,11 @@ describe('utils path', function () {
     expect(result).toBeTruthy()
   })
 
+  it('should match this path', function () {
+    const result = utils.checkPath('/applet-categories/7e74e7a8-a994-46c3-a79e-25ae44791983', '/applet-categories/{appletCategoryId}')
+    expect(result).toBeTruthy()
+  })
+
   it('should match a path with dashes 2', function () {
     const result = utils.checkPath('/workplaces/bsapi-workplace/groups/b0c535f5-1b8e-4388-8819-f73a9bda652d/modules/269198e4-d36d-4236-8b5a-f4a76bcb1f43', '/workplaces/{workplaceRef}/groups/{groupid}/modules/{id}')
     expect(result).toBeTruthy()
@@ -51,5 +55,56 @@ describe('utils path', function () {
   it('should match a path with two vars', function () {
     const result = utils.checkPath('/test/12-34-56/nested/09-87-65', '/test/{id}/nested/{nestedid}')
     expect(result).toBeTruthy()
+  })
+})
+
+describe('utils updateOperation', function () {
+  beforeEach(() => {
+    utils.logError = jest.fn()
+    utils.setSchema({
+      servers: [
+        {
+          url: 'http://127.0.0.1:3000'
+        }
+      ],
+      paths: {
+        '/kittens': {
+          delete: {
+            responses: {
+              404: {
+              }
+            }
+          }
+        }
+      }
+    })
+  })
+
+  it('should process a valid operation', function () {
+    const result = utils.updateOperation('http://127.0.0.1:3000/kittens', 'DELETE', '404')
+    expect(result).toBeDefined()
+    expect(utils.logError).not.toHaveBeenCalled()
+  })
+
+  it('should not process an operation with missing server', function () {
+    const result = utils.updateOperation('http://my-server0/kittens', 'DELETE', '404')
+    expect(result).toBeUndefined()
+    expect(utils.logError).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not process an operation with missing path', function () {
+    const result = utils.updateOperation('http://127.0.0.1:3000/dogs', 'DELETE', '404')
+    expect(result).toBeUndefined()
+    expect(utils.logError).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not process an operation with missing response', function () {
+    const result = utils.updateOperation('http://127.0.0.1:3000/kittens', 'DELETE', '401')
+    expect(result).toBeDefined()
+    expect(result).toHaveProperty('matchMethod')
+    expect(result.matchMethod).toHaveProperty('responses')
+    expect(result.matchMethod.responses).toHaveProperty('401')
+    expect(result.matchMethod.responses['401']).toMatchObject({ isUnexpected: true })
+    expect(utils.logError).toHaveBeenCalledTimes(1)
   })
 })
